eaab7d522379422ed17f5f27cbfbdf63e6fa8273
TheJasun <31616478+TheJasun@users.noreply.github.com>
2018-01-25 14:45:22 +0800

    Initial commit

---
+++ Assets/scripts/CarControl.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarControl : MonoBehaviour {
+
+    Rigidbody rb;
+
+    delegate void Testinput();
+    Testinput testInput;
+
+	void Start () {
+        rb = this.GetComponent<Rigidbody>();
+
+        testInput += Steer;
+        testInput += Drive;
+	}
+	
+	void Update () {
+        testInput();
+	}
+
+    float KeyboardInput()
+    {
+        float steer = Input.GetAxis("Steer");
+        return steer;
+    }
+
+    void Steer()
+    {
+        float rotation = rb.rotation.eulerAngles.y;
+
+        if (KeyboardInput() != 0)
+        {
+            if (KeyboardInput() < 0)
+            {
+                rotation -= 1f;
+            }
+            else rotation += 1f;
+
+            Quaternion rotQuart = Quaternion.AngleAxis(rotation, Vector3.up);
+
+            rb.MoveRotation(rotQuart);
+        }
+    }
+
+    void Drive()
+    {
+        if(Input.GetButton("Accelerate"))
+        {
+            rb.AddRelativeForce(Vector3.forward * 30f);
+        }
+        else if (Input.GetButton("Brake"))
+        {
+            Vector3 velo = rb.velocity;
+            if(velo.z > 0)
+            rb.AddRelativeForce(Vector3.back * 10f);
+        }
+    }
+}
---
+++ Assets/scripts/PickupSpin.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupSpin : MonoBehaviour {
+
+    private Transform tr;
+
+	void Start () {
+        tr = this.transform;
+        StartCoroutine(Spin());
+	}
+	
+    IEnumerator Spin()
+    {
+        while (true)
+        {
+            Vector3 rot = tr.rotation.eulerAngles;
+            rot.y += 2f;
+            Vector3 newRot = rot;
+            Quaternion newquat = Quaternion.Euler(newRot);
+            tr.rotation = newquat;
+            //bug.Log(rot);
+            yield return new WaitForSeconds(Time.deltaTime / 2);
+        }
+       
+    }
+}
